a285a7ec502b0d46276ebbedc30c3535c71a8573
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2015-09-18 19:00:18 +0200

    added concat for js, added every question to data set

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
 
 // watch files for changes and reload
-gulp.task('serve', function() {
+gulp.task('serve', ['scripts'], function() {
   browserSync({
     server: {
       baseDir: ''
@@ -10,4 +11,13 @@ gulp.task('serve', function() {
   });
 
   gulp.watch(['*.html', 'styles/*.css'], browserSync.reload);
+  gulp.watch(['scripts/*.js'], ['scripts']);
+});
+
+// concat js files
+gulp.task('scripts', function() {
+	return gulp
+			.src(['scripts/dataSets.js', 'scripts/components.js', 'scripts/app.js'])
+			.pipe(concat('app.js'))
+			.pipe(gulp.dest('dist/'));
 });
