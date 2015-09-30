9e24bfea855ed0ca4f129a18d698d04c5987b123
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2015-09-30 03:50:10 +0200

    search feature

--- dist/app.js
+++ dist/app.js
@@ -523,11 +523,30 @@ var headerComponent = {
 		}
 	},
 
+	_searchAll: function _searchAll(event) {
+		var questions = document.getElementsByClassName('name');
+		var keyword = document.getElementById('search').value;
+
+		var i = 0;
+		for (; i < questions.length; i++) {
+			if (questions[i].title.toLowerCase().indexOf(keyword) === -1) {
+				questions[i].parentNode.remove();
+			}
+		}
+
+		if (event.keyCode == 8) {
+			document.getElementById('search').value = '';
+			pageLoad(homePage, true);
+		}
+	},
+
 	render: function render() {
 		var headerData = JSON.parse(localStorage.getItem('headerData'))[0];
 		var content;
 
-		content = '<div class="expand-all" onClick="headerComponent._expandAll()">Expand all</div> \
+		content = '<input id="search" type="text" onKeyUp="headerComponent._searchAll(event)" placeholder="Search for questions..." /> \
+					<i class="material-icons search-icon">search</i> \
+					<div class="expand-all" onClick="headerComponent._expandAll()">Expand all</div> \
 					<div class="resource">Resource: \
 						<br /> \
 			  			<a href="' + headerData.resource + '">' + headerData.resourceName + '</a> \
--- src/client/scripts/components/pageComponents/HeaderComponent.js
+++ src/client/scripts/components/pageComponents/HeaderComponent.js
@@ -13,14 +13,32 @@ var headerComponent = {
 		for (; i < answers.length; i++) {
 			answers[i].className = 'answerContainer active';
 		}
-		
+	},
+
+	_searchAll(event) {
+		var questions = document.getElementsByClassName('name');
+		var keyword = document.getElementById('search').value;
+
+		var i = 0;
+		for (; i < questions.length; i++) {
+			if (questions[i].title.toLowerCase().indexOf(keyword) === -1) {
+				questions[i].parentNode.remove();
+			}
+		}
+
+		if (event.keyCode == 8) {
+			document.getElementById('search').value = '';
+			pageLoad(homePage, true);
+		}
 	},
 
 	render() {
 		var headerData = JSON.parse(localStorage.getItem('headerData'))[0];
 		var content;
 
-		content = '<div class="expand-all" onClick="headerComponent._expandAll()">Expand all</div> \
+		content = '<input id="search" type="text" onKeyUp="headerComponent._searchAll(event)" placeholder="Search for questions..." /> \
+					<i class="material-icons search-icon">search</i> \
+					<div class="expand-all" onClick="headerComponent._expandAll()">Expand all</div> \
 					<div class="resource">Resource: \
 						<br /> \
 			  			<a href="'+ headerData.resource +'">'+ headerData.resourceName +'</a> \
